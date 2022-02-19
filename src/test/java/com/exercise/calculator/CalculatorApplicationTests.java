a0aae8771cc02480bf5b7b5b3d200c10ed64b59a
maar0003 <miguel.alvr@gmail.com>
2022-02-19 11:23:03 +0100

    First commit

---
+++ src/test/java/com/exercise/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
